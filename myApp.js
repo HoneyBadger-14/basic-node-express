b60417a3bddac3a44c5d50bb30057ec414044741
Puneet Gaddi <puneetgaddi14@gmail.com>
2022-06-27 00:07:37 +0000

    Adding routes to root and json path

--- myApp.js
+++ myApp.js
@@ -1,6 +1,18 @@
 let express = require('express');
 let app = express();
 
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  //res.send("Hello Express");
+  console.log(process.env.MESSAGE_STYLE);
+  res.sendFile( __dirname + "/views/index.html" );
+});
+
+app.get("/json", (req, res) => {
+  let msg = process.env.MESSAGE_STYLE === "uppercase" ? {"message": "HELLO JSON"} : {"message": "Hello json"};
+  res.json(msg);
+})
 console.log("Hello World");
 
 
